2c05985b4e67b310ac89d8f398358f3511a35d84
ypau <ypau@users.noreply.github.com>
2021-08-19 13:46:37 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.82abad8c4a5b383124cafa59c0205f66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd23b153d0709979363cdcdef2aeed57",
+    "url": "https://ypau.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "1b6eabf1940f7176a9df",
+    "url": "https://ypau.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "dd444de4d7ed0cbfa07e",
+    "url": "https://ypau.github.io/task-management-frontend/static/js/2.983a10e5.chunk.js"
+  },
+  {
+    "revision": "1b6eabf1940f7176a9df",
+    "url": "https://ypau.github.io/task-management-frontend/static/js/main.e627b28b.chunk.js"
+  },
+  {
+    "revision": "f38a0b409c0e41b16de3",
+    "url": "https://ypau.github.io/task-management-frontend/static/js/runtime~main.5387c498.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://ypau.github.io/task-management-frontend/precache-manifest.82abad8c4a5b383124cafa59c0205f66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://ypau.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
